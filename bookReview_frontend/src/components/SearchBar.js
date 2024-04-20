8f11592426e10e35277738919389226761e56875
Vivek14kr <vivekkumarvky13@gmail.com>
2024-04-20 17:36:08 +0530

    search bar logic  imporoved

--- bookReview_frontend/src/components/SearchBar.js
+++ bookReview_frontend/src/components/SearchBar.js
@@ -9,29 +9,28 @@ const SearchBar = () => {
   const [input, setInput] = useState(search || "");
 
 
- const debouncedSearch = debounce((query) => {
-   if (query.length >= 3 || query.length === 0) {
-     const queryParam = {
-       page: 1,
-       sort,
-       order,
-       ...(query && { search: encodeURIComponent(query) }), 
-     };
+const debouncedSearch = debounce((query) => {
 
-  
-     Object.keys(queryParam).forEach((key) => {
-       if (!queryParam[key]) {
-         delete queryParam[key];
-       }
-     });
+  if (query.length >= 3 || query.length === 0) {
+    const queryParams = new URLSearchParams();
 
+    if (query.length > 0) {
+      queryParams.set("search", query);
+    }
+    queryParams.set("page", "1");
+    if (sort && order) {
+      queryParams.set("sort", sort);
+      queryParams.set("order", order);
+    }
 
-     router.push({
-       pathname: "/",
-       query: queryParam,
-     });
-   }
- }, 300);
+   
+    router.push({
+      pathname: "/",
+      query: queryParams.toString(),
+    });
+  }
+}, 300);
+
 
   useEffect(() => {
    setInput(search || "");
