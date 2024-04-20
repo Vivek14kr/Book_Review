6f83d254c4ed51d437e9554083e44f387bc63c83
Vivek14kr <vivekkumarvky13@gmail.com>
2024-04-20 17:07:52 +0530

    url updated

--- bookReview_frontend/src/hooks/useAuth.js
+++ bookReview_frontend/src/hooks/useAuth.js
@@ -28,7 +28,7 @@ export const AuthProvider = ({ children }) => {
 const login = async (credentials) => {
   setLoading(true);
   try {
-    const res = await fetch("http://localhost:3000/api/users/login", {
+    const res = await fetch("https://shy-teal-abalone-robe.cyclic.app/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(credentials),
@@ -53,7 +53,7 @@ const login = async (credentials) => {
   const signUp = async (userData) => {
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:3000/api/users/register", {
+      const res = await fetch("https://shy-teal-abalone-robe.cyclic.app/api/users/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(userData),
