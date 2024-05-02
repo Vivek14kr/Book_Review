8c44f8f908a0c086d409e3fb30ff4c0422b5afdc
Vivek Kumar <72031992+Vivek14kr@users.noreply.github.com>
2024-05-02 16:11:32 +0530

    Update Navbar.js

--- bookReview_frontend/src/components/Navbar.js
+++ bookReview_frontend/src/components/Navbar.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useAuth } from "@/hooks/useAuth";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    // Add scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <nav className="bg-gray-800 p-4 text-white flex flex-wrap justify-between items-center">
-      <div className="flex flex-wrap items-center">
+    <nav
+      className={`fixed top-0 w-full z-10 bg-gray-800 ${
+        isScrolled ? "shadow-md" : ""
+      } transition-shadow duration-300 p-4 text-white flex justify-between items-center`}
+    >
+      <div className="flex items-center">
         <Link
           className="text-white px-3 py-2 rounded-md text-sm font-medium mr-4"
           href="/"
@@ -47,7 +70,7 @@ const Navbar = () => {
             className="text-white px-3 py-2 rounded-md text-sm font-medium"
             href="/sign-up"
           >
-         Signup
+            Signup
           </Link>
         </div>
       )}
